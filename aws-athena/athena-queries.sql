6eec22e6b6f357589ae1a1ba3eade7b273045003
Vitor Antoni <v.silva.antoni@gmail.com>
2023-09-21 15:34:07 -0300

    aws-athena

---
+++ aws-athena/athena-queries.sql
@@ -0,0 +1,31 @@
+SELECT *
+FROM "{table}"
+LIMIT 10;
+
+SELECT "nome", "sobrenome"
+FROM "{table}"
+WHERE "salario" > 5000;
+
+SELECT "nome","sobrenome"
+FROM "{table}"
+WHERE "salario" > 3000
+ORDER BY("salario") DESC;
+
+SELECT "nome", "estado"
+FROM "{table}"
+WHERE "estado" = 'Rio Grande do Sul'
+AND "salario" >= 5500;
+
+SELECT count(*), "cargo"
+FROM "{table}"
+GROUP BY("cargo");
+
+SELECT count(*), "estado"
+FROM "{table}"
+GROUP BY("estado");
+
+/* Consultas AWS Athena Integracao de DataSources */
+select *
+from "Datasource1"."database"."table" as t1
+join "Datasource2"."database"."table" as t2 ON cast(t2.policyid as integer)=t1.policyid
+where {conditions}
