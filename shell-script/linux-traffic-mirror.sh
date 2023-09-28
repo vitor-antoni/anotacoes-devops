47d29584f129d4d5a005f8b4daf8aaa915c24f15
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2023-09-28 11:14:51 -0300

    Update linux-traffic-mirror.sh

--- shell-script/linux-traffic-mirror.sh
+++ shell-script/linux-traffic-mirror.sh
@@ -8,6 +8,7 @@
 
 ## Comandos para realizar na instância de monitoramento
 # ip_privado da instância de monitoramento
-ip link add vxlan0 type vxlan id 100 dev eth0 local {ip_privado} dstport 4789
+# 1904 eh o identificador da vlan
+ip link add vxlan0 type vxlan id 1904 dev eth0 local {ip_privado} dstport 4789
 ip link set vxlan0 up
 tcpdump -lnvXi vxlan0 icmp
