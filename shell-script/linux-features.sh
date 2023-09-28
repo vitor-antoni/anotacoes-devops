124bc1dc03168f806083e3a54647db2ef8f020dd
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2023-09-28 09:31:22 -0300

    Update linux-features.sh

--- shell-script/linux-features.sh
+++ shell-script/linux-features.sh
@@ -9,3 +9,12 @@ yum info {packageName}    # Informações sobre packages
 yum repolist              # Apresenta os repositórios existentes.
 yum update                # Atualiza o repositório
 yum upgrade -y            # Atualiza as packages
+
+# Encontrar arquivo e excluir
+find / -name {fileName} | xargs rm -rf
+
+# Encontrrar palavra em arquivo recursivamente
+grep -niR {diretorio}
+
+# Acha palavra em arquivo ou lista
+amazon-linux-extras list | grep {php}
