13933b0d4423a5a2eb6f8e6a0f697bc58cd6c2d6
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2023-09-28 08:17:02 -0300

    Update and rename instance-commands.sh to kubectl.sh

--- aws-eks/kubectl.sh
+++ aws-eks/kubectl.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Para exibir informacoes sobre o cluster
+kubectl get all -n {nomeNamespace}
+
+# Criar configurações
+kubectl create -f {path/para/o/arquivo} -n {nomeNamespace}
+
 # Nodes
 kubectl get nodes
 kubectl describe nodes {nodeName}
 
 # Namespace
-kubectl create namespace oc-giropops     ## Cria uma namespace chamada "oc-giropops"
-kubectl get namespace                    ## Lista todas as namespaces
+kubectl create namespace oc-giropops                           ## Cria uma namespace chamada "oc-giropops"
+kubectl get namespace                                          ## Lista todas as namespaces
+kubectl config set-context --current --namespace=oc-giropops      ## Para tornar o namespace oc-giropops o namespace padrao
 
 # Pods
 kubectl get pods                 ## Exibir todos os pods na namespace padrão
@@ -15,6 +22,7 @@ kubectl get pods -A
 kubectl get pods -A -o wide      ## Para saber em qual node está rodando e seu IP
 kubectl describe pods {podName}  ## Informações a respeito de um pod
 kubectl delete pods {podName}    ## Para deletar pods
+kubectl exec -it {podName} -- bash        ## Executar comandos no POD
 kubectl run {podName | nginxApp} --image {imageName | nginx} --port {port | 80}    ## Para rodar uma imagem de container
 kubectl expose pod {podName | nginx} --type NodePort      ## Expoem a porta 80 do pod, usando a porta alta do node
 
@@ -22,6 +30,7 @@ kubectl expose pod {podName | nginx} --type NodePort      ## Expoem a porta 80 d
 kubectl edit deployment         ## Editar configurações de Deployment
 kubectl get deployments         ## Mostra os deployments criados na namespace padrão
 kubectl get deployments -n oc-giropops         ## Mostra os deployments criados na namespace oc-giropops
+kubectl describe deployment {deploymentName}
 kubectl delete deploy {deployName}   ## Para excluir um deploy
 kubectl create deployment {deploymentName} --image {imageName | nginx} --replicas {ammount | 10}    ## Cria um deployment na namespace padrão/default
 kubectl create deployment {deploymentName} --image {imageName | nginx} --replicas {ammount | 10} -n {namespaceName | oc-giropops}   ## Cria um deployment na namespace oc-giropops
@@ -29,3 +38,9 @@ kubectl create deployment {deploymentName} --image {imageName | nginx} --replica
 # Service
 kubectl edit service            ## Para editar o serviço
 kubectl get service             ## Para listar os serviços
+
+# Replicaset
+kubectl get replicaset
+
+# Replicationcontroller
+kubectl get replicationcontroller
