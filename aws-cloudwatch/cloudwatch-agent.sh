77e6b047c2da6ae8fffa55abf7667d5a8e88560b
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2023-11-02 14:52:23 -0300

    Create cloudwatch-agent.sh

---
+++ aws-cloudwatch/cloudwatch-agent.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo yum install -y amazon-cloudwatch-agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+
+# default - 1
+# default - 1
+# default - 1
+# no - 2
+# no - 2
+# default - 1
+# default - 1
+# default - 1
+# default - 1
+# default - 1
+# standard - 2
+# default - 1
+# no - 2
+# no - 2
+# no - 2
+# no - 2
+
+# Copiar o JSON Output para os arquivos nos arquivos JSON nos diretórios abaixo:
+/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+/opt/aws/amazon-cloudwatch-agent/bin/config.json
+
+systemctl restart amazon-cloudwatch-agent
+systemctl status amazon-cloudwatch-agent
