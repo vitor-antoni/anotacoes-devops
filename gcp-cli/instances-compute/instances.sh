e22f6a288011f5f3819acc0bf506866fabe52f03
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2024-03-07 12:05:08 -0300

    Create instances.sh

---
+++ gcp-cli/instances-compute/instances.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Para criar uma máquina a partir de uma Imagem de máquina em outro projeto
+# Útil para cenários de DR (Dister Recovery)
+# Ref https://cloud.google.com/sdk/gcloud/reference/compute/instances/create
+gcloud compute instances create dcg01-test \
+--project={project} \
+--zone={zone} \
+--source-machine-image=projects/{project}/global/machineImages/{amiID} \
+--service-account={accountEmail} \
+--network-interface network={vpcName},subnet=back-wan \
+--network-interface network={vpcName},subnet=back-lan,external-ipv4-address=no-address
+
+# Para resetar senha de uma máquina Windows
+# Ou criar um novo usuário local
+gcloud compute reset-windows-password {vmNAME}
