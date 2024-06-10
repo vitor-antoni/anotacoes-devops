85b90311ad4c75bd38568135bc2d314e60dcfd2e
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2024-06-10 17:18:35 -0300

    Update bash.sh

--- shell-script/git-commands/bash.sh
+++ shell-script/git-commands/bash.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-## BRANCH
+git log                                               # Log 
 git pull                                              # Baixa atualizações
-git checkout {branchName}                             # Troca de branch
-git checkout -b {newBranchName}                       # Cria branch nova
 git add {modifiedFileName}                            # Adiciona o arquivo ao commit
 git commit -m 'Commit message'                        # Cria o commit com mensagem
 git push --set-upstream origin {currentBranchName}    # Envia atualizações
+git checkout {branchName}                             # Troca de branch
+git checkout -b {newBranchName}                       # Cria branch nova
+git checkout {branchName} -- {workingFileDirectory}   # Descarta alterações e puxa o mesmo arquivo de outra Branch
+git cherry-pick {commitId}                            # Baixa as alterações de um commit
