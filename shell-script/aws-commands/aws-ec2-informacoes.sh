e63116009e5d6de0cd281da5cd4200d40c16ec6a
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2025-06-24 21:53:06 -0300

    Update aws-ec2-informacoes.sh

--- shell-script/aws-commands/aws-ec2-informacoes.sh
+++ shell-script/aws-commands/aws-ec2-informacoes.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Para explorar as inforamções da instância
+# Para consultar informações sobre instãncias usamos o IMDS
+# e existem duas versões, para atualizá-la, acesse as seguintes opções
+# Actions > Instance Settings > Modify instance metadata options
+
+# Para explorar as inforamções da instância se IMDSv1
 curl http://169.254.169.254/latest/meta-data
+
+# Para explorar as inforamções da instância se IMDSv2
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+curl -H "X-aws-ec2-metadata-token: $TOKEN" 169.254.169.254/latest/meta-data/
 
 # Exemplo de capturação de informação do ID da instância.
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
